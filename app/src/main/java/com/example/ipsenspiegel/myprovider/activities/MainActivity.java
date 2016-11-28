23ac13d6aa1ca4c21ffbd48f607c5e212fb192bd
John <serradillamate.alejandro@gmail.com>
2016-11-28 12:00:45 +0100

    metidos datos DB

--- app/src/main/java/com/example/ipsenspiegel/myprovider/activities/MainActivity.java
+++ app/src/main/java/com/example/ipsenspiegel/myprovider/activities/MainActivity.java
@@ -1,27 +1,41 @@
 package com.example.ipsenspiegel.myprovider.activities;
 
+import android.content.ContentValues;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.ipsenspiegel.myprovider.R;
+import com.example.ipsenspiegel.myprovider.data.MyContentProvider;
 import com.example.ipsenspiegel.myprovider.data.MyDatabaseHelper;
+import com.example.ipsenspiegel.myprovider.data.paramsDb;
 
 import static android.R.attr.button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private EditText edtName;
+    private EditText edtAge;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        this.edtName = (EditText) findViewById(R.id.et_name);
+        this.edtAge = (EditText) findViewById(R.id.et_age);
+
         final Button btnQueryDb = (Button) findViewById(R.id.btn_query_db);
         btnQueryDb.setOnClickListener(this);
-        final Button btnAddDb = (Button)findViewById(R.id.btn_add_db);
+        final Button btnAddDb = (Button) findViewById(R.id.btn_add_db);
         btnQueryDb.setOnClickListener(this);
-        final Button btnDeleteDb = (Button)findViewById(R.id.btn_delete_db);
+        final Button btnDeleteDb = (Button) findViewById(R.id.btn_delete_db);
         btnQueryDb.setOnClickListener(this);
 
     }
@@ -29,16 +43,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View whichView) {
 
-        if (whichView.getId()== R.id.btn_query_db){
+        if (whichView.getId() == R.id.btn_query_db) {
 
-            MyDatabaseHelper dbHelper = new MyDatabaseHelper(this);
-            dbHelper.getWritableDatabase();
+        } else if (whichView.getId() == R.id.btn_add_db) {
 
-        }else if (whichView.getId()== R.id.btn_add_db){
+            ContentValues insertValues = new ContentValues();
+            insertValues.put(paramsDb.STUDENT_NAME, this.edtName.getText().toString());
+            insertValues.put(paramsDb.STUDENT_AGE, this.edtAge.getText().toString());
+            //como usamos nuestro content provider?
+            Uri insertUri = getContentResolver().insert(MyContentProvider.CONTEXT_URI, insertValues);
 
-        }else if (whichView.getId()== R.id.btn_delete_db){
+        } else if (whichView.getId() == R.id.btn_delete_db) {
+
+
+        } else {
 
         }
-
     }
 }
---
+++ app/src/main/java/com/example/ipsenspiegel/myprovider/data/MyContentProvider.java
@@ -0,0 +1,67 @@
+package com.example.ipsenspiegel.myprovider.data;
+
+import android.content.ContentProvider;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.support.annotation.Nullable;
+
+public class MyContentProvider extends ContentProvider {
+
+    private static final String TAG = MyContentProvider.class.getSimpleName();
+    private static final String PROVIDER_AUTHORITY = "com.example.ipsenspiegel.myprovider.data";
+    public static final Uri CONTEXT_URI = Uri.parse("content://" + MyContentProvider.PROVIDER_AUTHORITY);
+
+    private MyDatabaseHelper myDatabaseHelper;
+
+
+    public MyContentProvider() {
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        // Implement this to handle requests to delete one or more rows.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        SQLiteDatabase db = this.myDatabaseHelper.getWritableDatabase();
+
+        // se puede a lo bruto pero teniendo abstracciones
+        //db.execSQL("INSERT INTO ");
+
+        long regID = db.insert(paramsDb.TABLE_NAME, null, values);
+
+        return ContentUris.withAppendedId(MyContentProvider.CONTEXT_URI, regID);
+    }
+
+    @Override
+    public boolean onCreate() {
+       this.myDatabaseHelper = new MyDatabaseHelper(this.getContext());
+        return true;
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection,
+                        String[] selectionArgs, String sortOrder) {
+        // TODO: Implement this to handle query requests from clients.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Nullable
+    @Override
+    public String getType(Uri uri) {
+        return null;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection,
+                      String[] selectionArgs) {
+        // TODO: Implement this to handle requests to update one or more rows.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
